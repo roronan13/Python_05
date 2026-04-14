5692c440a6911f3add11dfdbc4160ca4dcc742ab
roronan13 <ronan.pothier@gmail.com>
2026-04-14 19:01:21 +0200

    ok

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -5,6 +5,8 @@ from typing import Any
 
 
 class DataProcessor(ABC):
+    def __init__(self):
+        self.datas = []
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
@@ -17,12 +19,31 @@ class DataProcessor(ABC):
     def output(self) -> tuple[int, str]:
         
 
-
 class NumericProcessor(DataProcessor):
+
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, (int, float)):
+            return True
+        elif isinstance(data, list):
+            return all(isinstance(d, (int, float)) for d in data)
+        else:
+            return False
 
 
 class TextProcessor(DataProcessor):
 
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        elif isinstance(data, list):
+            return all(isinstance(d, str) for d in data)
+        else:
+            return False
 
 class LogProcessor(DataProcessor):
 
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            return all(isinstance(key, str) and isinstance(value, str) for key, value in data.items)
+        elif isinstance(data, list):
+            
