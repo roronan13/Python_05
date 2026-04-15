9e9e896be19cdaeed758cc72ccca9ea50d0ba956
roronan13 <ronan.pothier@gmail.com>
2026-04-15 19:47:28 +0200

    ok

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -28,6 +28,17 @@ class NumericProcessor(DataProcessor):
             return all(isinstance(d, (int, float)) for d in data)
         else:
             return False
+        
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if not self.validate(data):
+            raise Exception("Improper numeric data\n")
+        
+        if isinstance(data, (int, float)):
+            self.datas.append(str(data))
+
+        else:
+            for d in data:
+                self.datas.append(str(d))
 
 
 class TextProcessor(DataProcessor):
@@ -39,11 +50,34 @@ class TextProcessor(DataProcessor):
             return all(isinstance(d, str) for d in data)
         else:
             return False
+        
+    def ingest(self, data: str | list[str]) -> None:
+        if not self.validate(data):
+            raise Exception("Improper string data\n")
+        
+        if isinstance(data, str):
+            self.datas.append(data)
+
+        else:
+            for d in data:
+                self.datas.append(d)
+
 
 class LogProcessor(DataProcessor):
 
     def validate(self, data: Any) -> bool:
         if isinstance(data, dict):
-            return all(isinstance(key, str) and isinstance(value, str) for key, value in data.items)
+            return all(isinstance(key, str) and isinstance(value, str) for key, value in data.items())
         elif isinstance(data, list):
-            
+            if all(isinstance(d, dict) for d in data):
+                for d in data:
+                    if not all(isinstance(key, str) and isinstance(value, str) for key, value in d.items()):
+                        return False
+            else:
+                return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: dict | list[dict]) -> None:
+        
