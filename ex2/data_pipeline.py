298fdd4cc05d4436d59cdca225e553af802d0125
roronan13 <ronan.pothier@gmail.com>
2026-05-14 13:40:01 +0200

    bon

--- ex2/data_pipeline.py
+++ ex2/data_pipeline.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 from typing import Any
+from typing import Protocol
 
 
 class DataProcessor(ABC):
@@ -170,4 +171,3 @@ remaining {len(data_processor.datas)} on processor")
 
 
 if __name__ == "__main__":
-    
